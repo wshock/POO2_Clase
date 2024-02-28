3671a8619ce28eda6535a9f7125f21b272631a69
Laboratorio de Software <lds@unac.local>
2024-02-27 12:43:25 -1200

    creacion clase padre y una hijo

--- proyectMascotas/src/Mascota.java
+++ proyectMascotas/src/Mascota.java
@@ -1,3 +1,9 @@
 public class Mascota {
-    
+    private int id;
+    public String nombre;
+
+    public Mascota(int id, String nombre) {
+        this.id = id;
+        this.nombre = nombre;
+    }
 }
